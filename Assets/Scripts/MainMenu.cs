6417a2ebd7336c8d470b6262f278d5b09c53c5fb
Cresus83 <alan.thob@hotmail.fr>
2023-03-16 17:11:23 +0100

    add main menu (update)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject settingsWindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindows()
+    {
+        settingsWindow.SetActive(false);
+    }
+    public void CreditsButton()
+    {
+
+    }
+
+}
